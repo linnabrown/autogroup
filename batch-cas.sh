6256669afa9938f1b6d4b875a30a93b2f532fd9a
huangle <505103350@qq.com>
2019-01-19 22:59:37 +0800

    omg

--- batch-cas.sh
+++ batch-cas.sh
@@ -9,16 +9,18 @@ do
        for file_name in $dir/*
        do
            {
-               suffix=${file_name#*.}
-               if [ "$suffix" == "faa" ]; then
+               ${file_name#*.}
+               if [ "${file_name#*.}"x = "faa"x ]; then
                    faa="$file_name"
-               elif [ "$suffix" == "fna" ]; then
+               elif [ "${file_name#*.}"x = "fna"x ]; then
                    fna="$file_name"
-               else
+               elif ["${file_name#*.}"x = "gff"x]; then
                    gff="$file_name"
                fi
 
            }
        done
-       python acr_aca_cri_runner.py -n $fna -f $gff -a $faa -o /ssd/huangle/archaea_gff_faa_fna_output/${dir##*/}
-}&done
+       echo acr_aca_cri_runner.py -n $fna -f $gff -a $faa -o /ssd/huangle/archaea_gff_faa_fna_output/${dir##*/}
+  fi
+}
+done
