f69916d0c3b8f55ecff7a3669b699c1c569dbd33
huangle <505103350@qq.com>
2019-01-20 14:51:25 +0800

    :bug:

--- batch-cas.sh
+++ batch-cas.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
 
-
-for dir in /ssd/huangle/archaea_gff_faa_fna/*
+#
+for dir in /ssd/huangle/archaea_gff_faa_fna/*;
 do
 {
-  if [ -d "$dir" ]
-    then
-       for file_name in $dir/*
-       do
-           {
-               ${file_name#*.}
-               if [ "${file_name#*.}"x = "faa"x ]; then
-                   faa="$file_name"
-               elif [ "${file_name#*.}"x = "fna"x ]; then
-                   fna="$file_name"
-               elif ["${file_name#*.}"x = "gff"x]; then
-                   gff="$file_name"
-               fi
+  if [ -d "$dir" ]; then
 
-           }
-       done
-       echo acr_aca_cri_runner.py -n $fna -f $gff -a $faa -o /ssd/huangle/archaea_gff_faa_fna_output/${dir##*/}
+       echo acr_aca_cri_runner.py -n "$dir"_genomic.fna -f "$dir"_genomic.gff -a "$dir"_protein.faa -o /ssd/huangle/archaea_gff_faa_fna_output/${dir##*/}
   fi
 }
 done
